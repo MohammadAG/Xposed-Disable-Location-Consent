8999c7127981ff6e855eac975be3109dfcd2c3cc
zenzizenzic <insert.something.of.use.here@gmail.com>
2014-03-03 12:18:12 -0600

    Update to v1.2

--- src/com/mohammadag/disablelocationconsent/DisableLocationConsent.java
+++ src/com/mohammadag/disablelocationconsent/DisableLocationConsent.java
@@ -1,8 +1,7 @@
 package com.mohammadag.disablelocationconsent;
 
 import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
+import android.location.LocationManager;
 import android.os.Build;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -13,34 +12,20 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class DisableLocationConsent implements IXposedHookLoadPackage {
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-		// Android 4.4.x (KitKat) or higher
 		if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 			if (lpparam.packageName.equals("com.google.android.gms")) {
 				final Class<?> NetworkLocationService = XposedHelpers.findClass("com.google.android.location.network.NetworkLocationService", lpparam.classLoader);
+
 				XposedHelpers.findAndHookMethod("com.google.android.location.network.NetworkLocationService", lpparam.classLoader, "a", Context.class, new XC_MethodReplacement() {
 					@Override
 					protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
 						Context context = (Context) param.args[0];
-						Cursor cursor = null;
-						int networkLocationOptIn = 0;
-						try {
-							cursor = context.getContentResolver().query(Uri.parse("content://com.google.settings/partner"), new String[] { "value" }, "name=?", new String[] { "network_location_opt_in" }, null);
-							if (cursor != null && cursor.moveToNext()) {
-								networkLocationOptIn = cursor.getInt(0);
-							}
-						}
-						catch (Exception e) {
-							return null;
-						}
-						finally {
-							if (cursor != null) {
-								cursor.close();
-							}
 
-							if (networkLocationOptIn == 0) {
-								XposedHelpers.callStaticMethod(NetworkLocationService, "a", context, true);
-							}			
-						}
+						LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+						boolean networkProviderEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+						XposedHelpers.callStaticMethod(NetworkLocationService, "b", context, networkProviderEnabled);
+
 						return null;
 					}
 				});
@@ -48,33 +33,34 @@ public class DisableLocationConsent implements IXposedHookLoadPackage {
 		} else if (android.os.Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN_MR2) {
 			if (lpparam.packageName.equals("com.google.android.location")) {
 				final Class<?> NetworkLocationProvider = XposedHelpers.findClass("com.google.android.location.NetworkLocationProvider", lpparam.classLoader);
-				XposedHelpers.findAndHookMethod("com.google.android.location.NetworkLocationProvider", lpparam.classLoader, "applySettings", new XC_MethodReplacement() {
+				String hookedMethodName;
+
+				if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+					hookedMethodName = "applySettings";
+				} else {
+					hookedMethodName = "handleEnable";
+				}
+
+				XposedHelpers.findAndHookMethod("com.google.android.location.NetworkLocationProvider", lpparam.classLoader, hookedMethodName, new XC_MethodReplacement() {
 					@Override
 					protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
 						Context context = (Context) XposedHelpers.getStaticObjectField(param.thisObject.getClass(), "context");
+
 						if (context == null) {
 							return null;
 						}
 
-						Cursor cursor = null;
-						int networkLocationOptIn = 0;
-						try {
-							cursor = context.getContentResolver().query(Uri.parse("content://com.google.settings/partner"), new String[] { "value" }, "name=?", new String[] { "network_location_opt_in" }, null);
-							if (cursor != null && cursor.moveToNext()) {
-								networkLocationOptIn = cursor.getInt(0);
-							}
-						} catch (Exception e) {
+						Object NetworkLocationProviderInstance = XposedHelpers.callStaticMethod(NetworkLocationProvider, "getInstance");
+
+						if (NetworkLocationProviderInstance == null) {
 							return null;
-						} finally {
-							if (cursor != null) {
-								cursor.close();
-							}
-							
-							if (networkLocationOptIn == 0) {
-								Object NetworkLocationProviderInstance = XposedHelpers.callStaticMethod(NetworkLocationProvider, "getInstance");
-								XposedHelpers.callMethod(NetworkLocationProviderInstance, "userConfirmedEnable", true);
-							}			
 						}
+
+						LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+						boolean networkProviderEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+						XposedHelpers.callMethod(NetworkLocationProviderInstance, "setUserConfirmedPreference", networkProviderEnabled);
+
 						return null;
 					}
 				});
